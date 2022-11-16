92464f9e7d26beea4bbc54950cc8b10279201c0d
Anurag Rawat <anurag.rawat0201@gmail.com>
2022-11-16 22:20:51 +0000

    work in progress

--- Next_Shopping Bag Discount v5/solution.py
+++ Next_Shopping Bag Discount v5/solution.py
@@ -1,17 +1,41 @@
 class Solution:
+    def __init__(self) -> None:
+        self.items_dict = {}  # {item_code: [cost, quantity]}
+        # self.populate()
+
     def get_item_code(self, item: str) -> str:
         return item[:3]
 
     def get_item_cost(self, item: str) -> int:
         return int(item[3:])
 
-    def calculate_bag_total(self, items, discounts) -> int:
-        for discount_code in discounts:
-            pass
+    def populate(self, items):
+        for item in items:
+            item_cost = self.get_item_cost(item)
+            item_code = self.get_item_code(item)
 
-        # for item in items:
-        #     item_code = self.get_item_code(item)
-        #     item_cost = self.get_item_cost(item)
+            if item_code in self.items_dict:
+                self.items_dict[item_code][0] += item_cost
+                self.items_dict[item_code][1] += 1
+            else:
+                self.items_dict[item_code] = [item_cost, 1]
+
+        return self.items_dict
+
+    def calculate_bag_total(self, items, discounts):
+        items_dict = {}  # {item_code: [cost, quantity]}
+
+        for item in items:
+            item_cost = self.get_item_cost(item)
+            item_code = self.get_item_code(item)
+
+            if item_code in items_dict:
+                items_dict[item_code][0] += item_cost
+                items_dict[item_code][1] += 1
+            else:
+                items_dict[item_code] = [item_cost, 1]
+
+        return items_dict
 
     def is_qualified(self, item):
         return
@@ -22,7 +46,6 @@ class Solution:
     def __get_discount(self, item):
         pass
 
-    def is_valid(self) -> int:
-        '''
-        Is the discount valid?
-        '''
+
+sol = Solution()
+print(sol.calculate_bag_total(["ABC015", "DEF020", "ABC010"], ["ABC2P50", "DEF1C05"]))
