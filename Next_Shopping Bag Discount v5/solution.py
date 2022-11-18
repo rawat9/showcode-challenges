19f614c260834a5eaae0b2e4188b3ae8c73b1347
Anurag <anurag.rawat0201@gmail.com>
2022-11-18 18:41:25 +0000

    completed implementation

--- Next_Shopping Bag Discount v5/solution.py
+++ Next_Shopping Bag Discount v5/solution.py
@@ -1,7 +1,6 @@
 class Solution:
     def __init__(self) -> None:
         self.items_dict = {}  # {item_code: [cost, quantity]}
-        # self.populate()
 
     def get_item_code(self, item: str) -> str:
         return item[:3]
@@ -15,37 +14,38 @@ class Solution:
             item_code = self.get_item_code(item)
 
             if item_code in self.items_dict:
-                self.items_dict[item_code][0] += item_cost
-                self.items_dict[item_code][1] += 1
+                self.items_dict.get(item_code)[0] += item_cost
+                self.items_dict.get(item_code)[1] += 1
             else:
                 self.items_dict[item_code] = [item_cost, 1]
 
-        return self.items_dict
-
     def calculate_bag_total(self, items, discounts):
-        items_dict = {}  # {item_code: [cost, quantity]}
+        self.populate(items)
+        total_amount = 0
 
-        for item in items:
-            item_cost = self.get_item_cost(item)
-            item_code = self.get_item_code(item)
+        for discount_code in discounts:
+            item_code = self.get_item_code(discount_code) 
+            min_count = int(discount_code[3])
 
-            if item_code in items_dict:
-                items_dict[item_code][0] += item_cost
-                items_dict[item_code][1] += 1
-            else:
-                items_dict[item_code] = [item_cost, 1]
+            if (self.items_dict.get(item_code)[1] > min_count):
+                continue
 
-        return items_dict
+            discount = self.__get_discount(item_code, discount_code)
+            total_amount += self.items_dict.get(item_code)[0] - discount
 
-    def is_qualified(self, item):
-        return
+        return total_amount
 
     def __get_discount_category(self, discount_code):
-        return discount_code[3]
+        return discount_code[4]
 
-    def __get_discount(self, item):
-        pass
+    def __get_discount(self, item_code, discount_code):
+        discount_category = self.__get_discount_category(discount_code)
+        item_discount = int(discount_code[5:])
 
+        if (discount_category == 'P'):
+            return self.items_dict[item_code][0] * (item_discount / 100)
+        else:
+            return item_discount
 
 sol = Solution()
-print(sol.calculate_bag_total(["ABC015", "DEF020", "ABC010"], ["ABC2P50", "DEF1C05"]))
+print(sol.calculate_bag_total(["ABC010", "DEF020", "ABC010"], ["ABC1P50", "DEF1C05"]))
