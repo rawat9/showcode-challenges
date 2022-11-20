2f925a377b4e8281e4570eb58a34f19e66dbf31c
Anurag Rawat <anurag.rawat0201@gmail.com>
2022-11-20 19:29:04 +0000

    add question, in-progress solution

--- src/lloyds_saving_money/solution.py
+++ src/lloyds_saving_money/solution.py
@@ -1,6 +1,13 @@
 class Solution:
+    def __init__(self):
+        self.counter = 0
+
     def algo(self, start, end):
-        pass
+        self.counter += 1
+        if start < end:
+            return start + self.algo(start + 1, end)
+        else:
+            return end
 
     def number_of_days_to_save(self, moneySaved):
 
@@ -9,4 +16,9 @@ class Solution:
         # 3 + 4 + 5 + 6 + 7 + 8 + 9 = 35 - 2 + 9 = 42
         # 4 + 5 + 6 + 7 + 8 + 9 + 10 = 42 - 3 + 10 = 49
 
-        pass
+        return self.algo(2, 8)
+
+
+sol = Solution()
+print(sol.number_of_days_to_save(8), 4)
+print(sol.counter)
