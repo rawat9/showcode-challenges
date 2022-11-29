c1ba339e622b55de9026a2e5c154ea4940636c72
Anurag Rawat <anurag.rawat0201@gmail.com>
2022-11-29 21:25:57 +0000

    complete nationwide challenge

--- src/nationwide_hide_secure_card_details/solution.py
+++ src/nationwide_hide_secure_card_details/solution.py
@@ -1,51 +1,47 @@
 import re
+import cProfile
+import pstats
 
 
 class Solution:
     def __init__(self):
-        self.numbers_in_message = 0
+        self.message = ""
 
-    def get_numbers(self, message):
-        self.numbers_in_message = len(re.findall("[0-9]+", message))
-
-    def verify_card(self, length: int, prefix, message):
+    def verify_card(self, length: int, prefix):
         possibilities = []
 
         if isinstance(prefix, tuple):
             for p in prefix:
                 pattern = f"{p}[0-9]" + "{" + f"{length - len(p)}" + "}"
-                found = re.findall(pattern, message)
-                if found and self.numbers_in_message > 0:
-                    self.numbers_in_message -= len(found)
+                found = re.findall(pattern, self.message)
+                if found:
                     possibilities += found
 
+            self.message = self.update_message(possibilities, self.message)
             return possibilities
 
         pattern = f"{prefix}[0-9]" + "{" + f"{length - len(prefix)}" + "}"
-        found = re.findall(pattern, message)
-        if (
-            found
-            and self.numbers_in_message > 0
-            and len(found) <= self.numbers_in_message
-        ):
-            self.numbers_in_message -= len(found)
+        found = re.findall(pattern, self.message)
+        if found:
+            self.message = self.update_message(found, self.message)
             return found
 
         return []
 
     def get_card_details(self, message):
-        self.get_numbers(message)
-        isMasterCard = self.verify_card(16, ("51", "52", "53", "54", "55"), message)
-        isAmex = self.verify_card(15, ("34", "37"), message)
-        isVisa = self.verify_card(13, "4", message)
+        self.message = message
+
+        isMasterCard = self.verify_card(16, ("51", "52", "53", "54", "55"))
+        isAmex = self.verify_card(15, ("34", "37"))
+        isVisa = self.verify_card(13, "4")
 
         all_possibles = isMasterCard + isAmex + isVisa
         card_distributors = list(map(self.get_card_distributor, all_possibles))
 
         if not card_distributors:
-            return [["NONE"], all_possibles]
+            return ["NONE", self.message]
 
-        return [card_distributors, all_possibles]
+        return [*card_distributors, self.message]
 
     def get_card_distributor(self, card_number: str):
         card_distributor = "NONE"
@@ -62,13 +58,9 @@ class Solution:
         return card_distributor
 
     def redact_card_details(self, message):
-        return self.get_card_details(message)[0] + [self.get_encoded(message)]
+        return self.get_card_details(message)
 
-    def get_encoded(self, message):
-        card_numbers = self.get_card_details(message)[1]
-        if not card_numbers:
-            return message
-
+    def update_message(self, card_numbers, message):
         for card_number in card_numbers:
             message = message.replace(card_number, "*" * len(card_number))
 
